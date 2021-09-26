dc6ca10d5474fea49594d7e7c6e6d002167c820f
amir <atapour@users.noreply.github.com>
2021-09-26 20:42:46 +0100

    power law transform demo

---
+++ gamma_correction.py
@@ -0,0 +1,198 @@
+# ===================================================================
+
+# Example : power law transform (gamma correction) on a video file or live camera stream
+# specified on the command line (e.g. python gamma_correction.py video_file) or from an
+# attached web camera
+
+# Author : Amir Atapour Abarghouei, amir.atapour-abarghouei@durham.ac.uk
+
+# Copyright (c) 2021 Amir Atapour Abarghouei
+
+# based on : https://github.com/tobybreckon/python-examples-ip/blob/master/gamma.py
+# License : LGPL - http://www.gnu.org/licenses/lgpl.html
+
+# ===================================================================
+
+import cv2
+import argparse
+import math
+import numpy as np
+
+# ===================================================================
+
+keep_processing = True
+
+# parse command line arguments for camera ID or video file
+
+parser = argparse.ArgumentParser(
+    description='Perform Gamma Correction on camera/video image')
+
+parser.add_argument(
+    "--camera",
+    type=int,
+    help="specify camera to use",
+    default=0)
+
+parser.add_argument(
+    'video_file',
+    metavar='video_file',
+    type=str,
+    nargs='?',
+    help='specify optional video file')
+
+args = parser.parse_args()
+
+# ===================================================================
+
+# power law transform
+# image - colour image
+# gamma - "gradient" co-efficient of gamma function
+
+def powerlaw_transform(image, gamma):
+
+    # compute power-law transform
+    # not defined for pixel = 0 (!)
+
+    # 0-255 clipping taking care of overflow
+
+    image = np.clip(np.power(image, gamma), 0, 255).astype('uint8')
+
+    return image
+
+# ===================================================================
+
+# define video capture object
+
+print("Starting camera stream")
+cap = cv2.VideoCapture()
+
+# define display window name
+
+window_name = "Live Camera Input and Exponential Transform"  # window name
+
+# if command line arguments are provided try to read video_file
+# otherwise default to capture from attached H/W camera
+
+if (((args.video_file) and (cap.open(str(args.video_file))))
+        or (cap.open(args.camera))):
+
+    # create window by name
+
+    cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
+
+    # add track bar controllers for settings
+
+    gamma = 100  # default - no effect on the image
+    cv2.createTrackbar("gamma, (* 0.01)", window_name, gamma, 500, lambda x:x)
+
+    while (keep_processing):
+
+        # if video file or camera successfully open then read frame from video
+
+        if (cap.isOpened):
+            ret, frame = cap.read()
+
+            # when we reach the end of the video (file) exit cleanly
+
+            if (ret == 0):
+                keep_processing = False
+                continue
+
+        # start a timer (to see how long processing and display takes)
+
+        start_t = cv2.getTickCount()
+
+        # *******************************
+
+        # parameters for rescaling the image for easier processing
+
+        scale_percent = 70 # percent of original size
+        width = int(frame.shape[1] * scale_percent/100)
+        height = int(frame.shape[0] * scale_percent/100)
+        dim = (width, height)
+
+        # rescale image
+
+        frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+
+        # make a copy
+
+        gamma_img = frame.copy()
+
+        # get parameters from track bars
+
+        gamma = cv2.getTrackbarPos("gamma, (* 0.01)", window_name) * 0.01
+
+        # use power-law function to perform gamma correction
+
+        gamma_img = powerlaw_transform(gamma_img, gamma)
+
+        # parameters for overlaying text labels on the displayed images
+
+        font = cv2.FONT_HERSHEY_COMPLEX
+        bottomLeftCornerOfText = (10,height-15)
+        fontScale = 1
+        fontColor = (123,49,126)
+        lineType  = 6
+
+        # overlay corresponding labels on the images
+
+        cv2.putText(frame,'Original Input', 
+            bottomLeftCornerOfText, 
+            font, 
+            fontScale,
+            fontColor,
+            lineType)
+        cv2.putText(gamma_img,f'Power Law Transform - Gamma: {gamma}', 
+            bottomLeftCornerOfText, 
+            font, 
+            fontScale,
+            fontColor,
+            lineType)
+
+        # stack the images into a grid
+
+        output = cv2.hconcat([frame, gamma_img])
+
+        # *******************************
+
+        # display image
+
+        cv2.imshow(window_name, output)
+
+        # stop the timer and convert to ms. (to see how long processing and
+        # display takes)
+
+        stop_t = ((cv2.getTickCount() - start_t) /
+                  cv2.getTickFrequency()) * 1000
+
+        # start the event loop - essential
+
+        # cv2.waitKey() is a keyboard binding function (argument is the time in
+        # ms). It waits for specified milliseconds for any keyboard event.
+        # If you press any key in that time, the program continues.
+        # If 0 is passed, it waits indefinitely for a key stroke.
+        # (bitwise and with 0xFF to extract least significant byte of
+        # multi-byte response)
+
+        # wait 40ms or less depending on processing time taken (i.e. 1000ms /
+        # 25 fps = 40 ms)
+
+        key = cv2.waitKey(max(2, 40 - int(math.ceil(stop_t)))) & 0xFF
+
+        # It can also be set to detect specific key strokes by recording which
+        # key is pressed
+
+        # e.g. if user presses "x" then exit
+
+        if (key == ord('x')):
+            keep_processing = False
+
+    # close all windows
+
+    cv2.destroyAllWindows()
+
+else:
+    print("No video file specified or camera connected.")
+
+# ===================================================================
